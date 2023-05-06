5493848943e966f1a8fe938c5a511bf379a7f3a0
azertgyv <atagiyev077@gmail.com>
2023-05-06 21:30:09 +0400

    lesson5

---
+++ app.js
@@ -0,0 +1,49 @@
+// function capitalFinder (capital){
+//     if (capital === 'aze'){
+//         return  "baku"
+
+//     } else if (capital === 'tr')
+//     return "ankara"
+//     console.log(capital)
+// }
+
+// function translateFinder(par){
+// switch (par) {
+//     case 'en':
+//         return 'Welcome'
+
+//     case 'rus':
+//         return 'Dobri Pojalovat'
+
+//     case 'az':
+//         return 'Xos gelmisiniz!'
+
+
+//     default:
+//         break;
+// }
+// }
+// console.log(translateFinder('en'))
+
+// const greatherThan100 = arg => arg > 100
+// console.log(greatherThan100(200))
+
+
+// function surnameGenerator(surname, gender, lg) {
+//     switch (lg) {
+//         case 'ru':
+//             return gender === 'm' ? surname + 'ev' : surname + 'eva'
+
+//         case 'tr':
+//             return gender === "m" ? surname + "oglu" : surname + "qizi"
+//         case 'pr':
+//             return 'zade'
+
+
+
+//         default:
+//             break;
+//     }
+// }
+// console.log(surnameGenerator('ali','m','tr'))
+// console.log(surnameGenerator('ali','w','ru'))
